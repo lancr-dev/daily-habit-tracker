c72b3999048c4e5e1752092a435eb447fc741baa
Lance <lanceandreiducante@gmail.com>
2026-08-10 14:55:24 +0800

    initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: 'API is running..',
+  });
+});
+
+const startServer = () => {
+  try {
+    app.listen(PORT, () => {
+      console.log(`Server is running on PORT: ${PORT}`);
+    });
+  } catch (error) {
+    console.error(`Server connection failed: ${error.message}`);
+    process.exit(1);
+  }
+};
+
+startServer();
