2d1ce18f91ec36bd38d59dbc8c577200d19a4260
Lance <lanceandreiducante@gmail.com>
2026-08-14 07:04:33 +0800

    created services

---
+++ backend/src/services/habitService.js
@@ -0,0 +1,47 @@
+import Habit from '../config/models/Habit.js';
+
+export const createHabitService = async ({ habitName }) => {
+  const habit = await Habit.create({
+    habitName,
+  });
+
+  return habit;
+};
+
+export const getAllHabitService = async () => {
+  const habits = await Habit.find().sort({ createdAt: -1 });
+
+  return habits;
+};
+
+export const getHabitService = async (id) => {
+  const habit = await Habit.findById(id);
+
+  return habit;
+};
+
+export const updateHabitService = async (id, habitName) => {
+  const habit = await Habit.findById(id);
+
+  if (!habit) {
+    return null;
+  }
+
+  habit.habitName = habitName;
+
+  await habit.save();
+
+  return habit;
+};
+
+export const deleteHabitService = async (id) => {
+  const habit = await Habit.findById(id);
+
+  if (!habit) {
+    return null;
+  }
+
+  await habit.deleteOne();
+
+  return habit;
+};
