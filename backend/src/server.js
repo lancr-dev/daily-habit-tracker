2c02c97907d89ef8e28408e5a9bd88f717c93e06
Lance <lanceandreiducante@gmail.com>
2026-08-15 12:03:58 +0800

    configure cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,6 +9,13 @@ dotenv.config();
 
 const app = express();
 
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  }),
+);
+
 const PORT = process.env.PORT;
 
 app.use(express.json());
